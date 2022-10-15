fff06c853e6c1fb40cff199ac1d87fcd2e1960f2
Pierlauro Sciarelli <foss@pstux.dev>
2022-10-15 10:54:02 +0200

    match quoted "frame" to avoid false positives

--- lograr.js
+++ lograr.js
@@ -39,7 +39,7 @@ function inspectBF(){
     console.assert(appState);
 
     /* TODO allow dinamically modifying the regex from a textarea */
-    const BFregex = new RegExp("DBException::toString|fassert|assert\\.|frame|invariant[ |\(\)]|got signal|stacktrace|backtrace|failed to load|segmentation fault|error occurred|\"FAIL(URE)?\"|throwing exception", "i");
+    const BFregex = new RegExp("DBException::toString|fassert|assert\\.|\"frame\"|invariant[ |\(\)]|got signal|stacktrace|backtrace|failed to load|segmentation fault|error occurred|\"FAIL(URE)?\"|throwing exception", "i");
 
     const logLines = appState.log.lines;
 
